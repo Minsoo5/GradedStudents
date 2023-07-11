044d43462b3d034ad4f3d2b5b1f52d41896afe1e
min <minsoojo5@gmail.com>
2023-07-11 15:52:29 -0400

    Instance Variables and a small start on constructors

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,22 @@
 package io.zipcoder;
+import java.util.ArrayList;
 
 public class Student {
+
+    private String firstName;
+
+    private String lastName;
+
+    private ArrayList<Double> examScores;
+
+
+    public Student(String firstName, String lastName, Double[] testScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+    }
+
+
+
+
 }
