108b3c1fee0ac0369b31ca7c0899c604d51be00c
min <minsoojo5@gmail.com>
2023-07-12 07:52:08 -0400

    Student Class Finished?

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -7,16 +7,39 @@ public class Student {
 
     private String lastName;
 
-    private ArrayList<Double> examScores;
+    private ArrayList<Double[]> examScores;
 
 
     public Student(String firstName, String lastName, Double[] testScores) {
         this.firstName = firstName;
         this.lastName = lastName;
-
+        this.examScores.add(testScores);
     }
 
 
 
+    public String getFirstname() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public ArrayList<Double[]> getExamScores() {
+        return examScores;
+    }
+
+    public void setExamScores(Double[] testScores) {
+        this.examScores.add(testScores);
+    }
 
 }
